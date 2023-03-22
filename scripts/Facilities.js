4192d2604e34202a9c7d5dd7a33a2fa055cecbad
Curtis Webb <curtwebb1194@@gmail.com>
2023-03-22 15:28:30 -0500

    index is visible now

--- scripts/Facilities.js
+++ scripts/Facilities.js
@@ -1,21 +1,29 @@
-import { getFacilities, setFacility } from "./database.js";
+import { getFacilities, setFacility, getAvailableMinerals } from "./database.js";
 
 
 export const facilities = () => {
     const facilities = getFacilities()
-    // make buttons of the facilities
-    for (facility of facilities) {
-        let html = ""
-        html += `<select id = "facility"><button id="facilityButton">${facility.name}</button></select>`
-    }
+    let html = "<h4>Choose a Facility</h4>"
+    html += `<select id="facility">`
+    html += `<option value="0">Choose a Facility...</option>`
 
-  return html
+    const listItems = facilities.map(facility => {
+
+        return `<option value="${facility.id}">${facility.name}</option>`
+    })
+
+html += listItems.join("")
+html += "</select>"
+return html
 }
 
 document.addEventListener(
     "change",
     (event) => {
+        // when a facility is clicked,setFacility should take the value of the facility selected
         if (event.target.id === "facility") {
-            setFacility(parseInt(event.target.value))        }
+            setFacility(parseInt(event.target.value))
+
+        }
     }
 )
